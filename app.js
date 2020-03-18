ad31245e1f83f455fe0138243da35c946e6c1f28
Tarun Bansal <tarun.bansal@codalien.com>
2020-03-18 17:53:39 +0530

    port changed

--- app.js
+++ app.js
@@ -4,148 +4,148 @@ const qs = require('query-string');
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-    token: 'xoxb-977111493845-1008070455188-JEFZXq5FWMGsMDyO9AoRGtcL', //process.env.SLACK_BOT_TOKEN,
+    token: 'xoxb-977111493845-1008070455188-BmmF0oMnFIaEe8AKZohjGSqD', //process.env.SLACK_BOT_TOKEN,
     signingSecret: '7448e4d19c59e425553ba4e0269583af' //process.env.SLACK_SIGNING_SECRET
 });
 
-//simplata API Call
-function getMatchData(msg) {
-    const formData = {
-        'v': '0.1.0',
-        'api_key': '1957-9164-4593-7482',
-        'app_unique_name': 'integration-test-1',
-        'app': 'slack',
-        'data': msg,
-    }
+// //simplata API Call
+// function getMatchData(msg) {
+//     const formData = {
+//         'v': '0.1.0',
+//         'api_key': '1957-9164-4593-7482',
+//         'app_unique_name': 'integration-test-1',
+//         'app': 'slack',
+//         'data': msg,
+//     }
 
-    const config = {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }
+//     const config = {
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//         }
+//     }
 
-    axios.post('https://gols1.simplata.com/api/tokenize', qs.stringify(formData), config)
-        .then(function (response) {
-            if (response.status === "ok") {
-                console.log(response.status);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-// Listens to incoming messages that contain "hello"
-app.message(async ({ payload, context }) => {
-    const messages = payload.text;
-    try {
+//     axios.post('https://gols1.simplata.com/api/tokenize', qs.stringify(formData), config)
+//         .then(function (response) {
+//             if (response.status === "ok") {
+//                 console.log(response.status);
+//             }
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// }
+// // Listens to incoming messages that contain "hello"
+// app.message(async ({ payload, context }) => {
+//     const messages = payload.text;
+//     try {
 
-        const updateResult = await app.client.chat.update({
-            // The token you used to initialize your app is stored in the `context` object
-            token: 'xoxp-977111493845-977111953909-997048649987-a4a3e3fd90ca791dedb2c99c15e1fa8d',
-            // Payload message should be posted in the channel where original message was heard
-            channel: payload.channel,
-            ts: payload.ts,
-            // The user the message should appear for
-            user: payload.user,
-            text: 'Hi, I updated mesaage'
-        });
-
-
-        // Call the chat.postEphemeral method using the built-in WebClient
-        const result = await app.client.chat.postEphemeral({
-            // The token you used to initialize your app is stored in the `context` object
-            token: context.botToken,
-            // Payload message should be posted in the channel where original message was heard
-            channel: payload.channel,
-            // The user the message should appear for
-            user: payload.user,
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": `${messages}`
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*This looks like personal identifiable information (PII)*. Choose how to share it:"
-                    }
-                },
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Redact PII",
-                                "emoji": true
-                            },
-                            "action_id": "button_click"
-                        }
-                    ]
-                },
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Keep PII",
-                                "emoji": true
-                            }
-                        }
-                    ]
-                },
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Delete PII",
-                                "emoji": true
-                            }
-                        }
-                    ]
-                },
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Delete message",
-                                "emoji": true
-                            }
-                        }
-                    ]
-                }
-
-            ]
-        });
-    }
-    catch (error) {
-        console.error(error);
-    }
-});
+//         const updateResult = await app.client.chat.update({
+//             // The token you used to initialize your app is stored in the `context` object
+//             token: 'xoxp-977111493845-977111953909-997048649987-a4a3e3fd90ca791dedb2c99c15e1fa8d',
+//             // Payload message should be posted in the channel where original message was heard
+//             channel: payload.channel,
+//             ts: payload.ts,
+//             // The user the message should appear for
+//             user: payload.user,
+//             text: 'Hi, I updated mesaage'
+//         });
 
 
-app.action('button_click', ({ body, ack, say }) => {
-    // Acknowledge the action
-    ack();
-    say(`<@${body.user.id}> clicked the button`);
-});
+//         // Call the chat.postEphemeral method using the built-in WebClient
+//         const result = await app.client.chat.postEphemeral({
+//             // The token you used to initialize your app is stored in the `context` object
+//             token: context.botToken,
+//             // Payload message should be posted in the channel where original message was heard
+//             channel: payload.channel,
+//             // The user the message should appear for
+//             user: payload.user,
+//             "blocks": [
+//                 {
+//                     "type": "section",
+//                     "text": {
+//                         "type": "mrkdwn",
+//                         "text": `${messages}`
+//                     }
+//                 },
+//                 {
+//                     "type": "section",
+//                     "text": {
+//                         "type": "mrkdwn",
+//                         "text": "*This looks like personal identifiable information (PII)*. Choose how to share it:"
+//                     }
+//                 },
+//                 {
+//                     "type": "actions",
+//                     "elements": [
+//                         {
+//                             "type": "button",
+//                             "text": {
+//                                 "type": "plain_text",
+//                                 "text": "Redact PII",
+//                                 "emoji": true
+//                             },
+//                             "action_id": "button_click"
+//                         }
+//                     ]
+//                 },
+//                 {
+//                     "type": "actions",
+//                     "elements": [
+//                         {
+//                             "type": "button",
+//                             "text": {
+//                                 "type": "plain_text",
+//                                 "text": "Keep PII",
+//                                 "emoji": true
+//                             }
+//                         }
+//                     ]
+//                 },
+//                 {
+//                     "type": "actions",
+//                     "elements": [
+//                         {
+//                             "type": "button",
+//                             "text": {
+//                                 "type": "plain_text",
+//                                 "text": "Delete PII",
+//                                 "emoji": true
+//                             }
+//                         }
+//                     ]
+//                 },
+//                 {
+//                     "type": "actions",
+//                     "elements": [
+//                         {
+//                             "type": "button",
+//                             "text": {
+//                                 "type": "plain_text",
+//                                 "text": "Delete message",
+//                                 "emoji": true
+//                             }
+//                         }
+//                     ]
+//                 }
+
+//             ]
+//         });
+//     }
+//     catch (error) {
+//         console.error(error);
+//     }
+// });
+
+
+// app.action('button_click', ({ body, ack, say }) => {
+//     // Acknowledge the action
+//     ack();
+//     say(`<@${body.user.id}> clicked the button`);
+// });
 
 (async () => {
     // Start your app
-    await app.start(process.env.PORT || 3000);
+    await app.start(80);
 
     console.log('⚡️ Bolt app is running!');
 })();
