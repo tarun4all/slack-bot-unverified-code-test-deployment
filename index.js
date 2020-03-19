d6afe000645da2981c59ea9f5e5961f4819ee45c
Tarun Bansal <tarun.bansal@codalien.com>
2020-03-19 17:13:35 +0530

    update message code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('express')()
 const bodyParser = require('body-parser')
 const axios = require('axios').default;
 const request = require("request");
+var qs = require('qs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -31,13 +32,32 @@ app.post('/', (req, res) => {
         //   }).then(function (response) {
         //     console.log(response);
         //   });
-        request.post({url:'https://slack.com/api/chat.update', formData: {
-            token: 'xoxp-854617017029-854607499008-1013698991174-ddf84b0ace301d7245f58bf770b2da5f',
-            channel: req.body.event.channel,
-            ts: req.body.event.ts,
-            text: 'Chat encrypted..'
-        }}, function optionalCallback(err, httpResponse, body) {
-            console.log('Upload successful!  Server responded with:  ', err, httpResponse, body);
+        // request.post({url:'https://slack.com/api/chat.update', formData: {
+        //     token: 'xoxp-854617017029-854607499008-1013698991174-ddf84b0ace301d7245f58bf770b2da5f',
+        //     channel: req.body.event.channel,
+        //     ts: req.body.event.ts,
+        //     text: 'Chat encrypted..'
+        // }}, function optionalCallback(err, httpResponse, body) {
+        //     console.log('Upload successful!  Server responded with:  ', err, httpResponse, body);
+        // });
+        const config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }
+        const formData = {
+                    token: 'xoxp-854617017029-854607499008-1011006921556-f78318cfa00fc179d774feaf2a1a85ad',
+                    channel: req.body.event.channel,
+                    ts: req.body.event.ts,
+                    text: 'Chat encrypted..'
+                };
+
+        axios.post('https://slack.com/api/chat.update', qs.stringify(formData), config)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
         });
     }
 });
